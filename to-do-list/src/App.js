64ff18b79047b4a4c79f0ec90a5dd1110a72631d
Ares DoDox <luyn14@gmail.com>
2019-04-02 22:31:26 +0700

    create react app

---
+++ to-do-list/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+import TodoItem from './components/TodoItem';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.todoItems = [
+      {title : 'DoDox', isComplete : true},
+      {title : 'AramLee'},
+      {title : 'KillLove'}
+    ];
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.todoItems.map((item,index) => 
+            <TodoItem key={index} item={item}/>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
