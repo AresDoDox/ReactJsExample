dd47a1c1279dbab20e8cb6ea928a039c8e986171
Ares DoDox <luyn14@gmail.com>
2019-04-06 00:21:22 +0700

    done app -  to do list

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -2,24 +2,98 @@ import React, { Component } from 'react';
 import './App.css';
 import TodoItem from './components/TodoItem';
 
+import checkAllImage from './img/check-all.svg';
+
 class App extends Component {
   constructor(){
     super();
-    this.todoItems = [
-      {title : 'DoDox', isComplete : true},
-      {title : 'AramLee'},
-      {title : 'KillLove'}
-    ];
+  
+    this.state = {
+      newItem: '',
+      todoItems: [
+        {title : 'DoDox', isComplete : true},
+        {title : 'AramLee'},
+        {title : 'KillLove'}
+      ]
+    };
+    
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onChange = this.onChange.bind(this);
   };
+  onItemClicked(item) {
+    return (event) => {
+      const isComplete = item.isComplete;
+      // const index = this.state.todoItems.indexOf(item);
+      const {todoItems} = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isComplete : !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      });
+    }
+  }
 
+  onKeyUp(event){
+    if(event.keyCode === 13){ //Enter key
+      let text = event.target.value;
+      if(!text){
+        return;
+      }
+
+      text = text.trim(); //hàm xóa tất cả dấu cách đầu và cuối
+      if(!text){
+        return;
+      }
+      const {todoItems} = this.state;
+      this.setState({
+        newItem: '',
+        todoItems: [
+          {title : text, isComplete: false},
+          ...todoItems
+        ]
+      });
+    }
+  }
+  onChange(event){
+    this.setState({
+      newItem: event.target.value
+    });
+  }
   render() {
+    const { todoItems, newItem } = this.state;
     return (
       <div className="App">
+        <div className="Header">
+          <img src={checkAllImage} />
+          <input 
+            type="text"  
+            placeholder="Gõ vào đây" 
+            value={newItem}
+            onChange={this.onChange}
+            onKeyUp={this.onKeyUp}
+          />
+        </div>
         {
-          this.todoItems.map((item,index) => 
-            <TodoItem key={index} item={item}/>
+          todoItems.length > 0 &&
+          todoItems.map((item,index) =>
+            <TodoItem 
+              key={index} 
+              item={item} 
+              onClick={this.onItemClicked(item)} 
+            />
           )
         }
+        {
+          todoItems.length === 0 &&
+            'Nothing here.....'
+        }
+        {/* Toán tử &&, Kiểm tra tồn tại phần tử nào trong mảng k? */}
       </div>
     );
   }
--- to-do-list/src/components/TodoItem.js
+++ to-do-list/src/components/TodoItem.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import './TodoItem.css';
+import classNames from 'classnames';
+
+//import img
+import checkImage from '../img/check.svg';
+import checkCompleteImage from '../img/check-complete.svg';
 
 class TodoItem extends Component {
     render() {
-        const { item } = this.props;
-        let className = 'TodoItem';
-        if(item.isComplete){
-            className += ' TodoItem-complete';
+        const { item , onClick } = this.props;
+        let url = checkImage;
+        if (item.isComplete){
+            url = checkCompleteImage;
         }
+
+        // let className = 'TodoItem';
+        // if(item.isComplete){
+        //     className += ' TodoItem-complete';
+        // }
         return(
-            <div className={className}>
+            // <div className={className}>
+            <div 
+                
+                className={classNames(
+                    'TodoItem',
+                    {
+                        'TodoItem-complete' : item.isComplete
+                    }
+                )}
+            >
+                <img src={url} onClick={onClick} />
                 <p>{this.props.item.title}</p>
             </div>
         );
